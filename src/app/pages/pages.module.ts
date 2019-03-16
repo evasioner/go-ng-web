02101874c8dae3d9b289801def2f410be928c40a
hyunsoo.lee <hyunsoo.lee@way2bit.com>
2019-03-16 14:52:12 +0900

    불필요 페이지 제거

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
-
 import { PagesComponent } from './pages.component';
-import { DashboardModule } from './dashboard/dashboard.module';
-import { ECommerceModule } from './e-commerce/e-commerce.module';
 import { PagesRoutingModule } from './pages-routing.module';
 import { ThemeModule } from '../@theme/theme.module';
-import { MiscellaneousModule } from './miscellaneous/miscellaneous.module';
 import { TestPageComponent } from './test-page/test-page.component';
 
 const PAGES_COMPONENTS = [
@@ -16,9 +12,6 @@ const PAGES_COMPONENTS = [
   imports: [
     PagesRoutingModule,
     ThemeModule,
-    DashboardModule,
-    ECommerceModule,
-    MiscellaneousModule,
   ],
   declarations: [
     ...PAGES_COMPONENTS,
